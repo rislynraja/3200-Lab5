51a6b4b41c4c39cf420b60b5a0cdd7256f5d553a
rislynraja <rislyn_ferona.raja@uconn.edu>
2025-05-02 00:06:21 -0400

    Delete k2025_04_22_One_MET_image directory

--- k2025_04_22_One_MET_image/app/src/main/java/com/example/k2025_04_22_one_met_image/models/DataModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.k2025_04_22_one_met_image.models
-
-data class DataModel(
-    val objectID: Int,
-    val title: String,
-    val artistDisplayName: String,
-    val primaryImage: String,
-    val primaryImageSmall: String
-)
